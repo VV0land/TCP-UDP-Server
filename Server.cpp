39401bc132421cfad35e6135b75d0f4ed675abc2
HonoRyzen7 <jobberj@bk.ru>
2025-11-20 14:11:27 +0500

    Добавлен TCP-сервер

--- Server.cpp
+++ Server.cpp
@@ -1,19 +1,33 @@
 ﻿//  18.11.2025
 //  Простеннький UDP-сервер.
+//  20.11.2025
+//  Добавлен TCP-сервер.
+
 
 #include <iostream>
 #include <string>
 #include <ws2tcpip.h>
+#include <set>
+#include <vector>
+
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 
 const int BUFFER_SIZE = 1024;
+const int MAX_CLIENTS = 100;
+
 fd_set readfds, allfds;
 SOCKET max_sd;
 int total_connections = 0;
 
+struct ClientData {
+    SOCKET fd;
+    sockaddr_in addr;
+    int addr_len;
+};
 
+std::vector<ClientData> clients;
 
 bool initWinsock() {
     WSADATA wsaData;
@@ -60,6 +74,70 @@ SOCKET createUdpServer(int port) {
     return server_fd;
 }
 
+SOCKET createTcpServer(int port) {
+    SOCKET server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server_fd == INVALID_SOCKET) {
+        std::cerr << "socket() failed with error: " << WSAGetLastError() << std::endl;
+        return INVALID_SOCKET;
+    }
+
+    if (!setNonBlocking(server_fd)) {
+        closesocket(server_fd);
+        return INVALID_SOCKET;
+    }
+
+    sockaddr_in server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if (bind(server_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        std::cerr << "bind() failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(server_fd);
+        return INVALID_SOCKET;
+    }
+
+    if (listen(server_fd, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "listen() failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(server_fd);
+        return INVALID_SOCKET;
+    }
+
+    std::cout << "TCP server started on port " << port << std::endl;
+    return server_fd;
+}
+
+void acceptTcpConnection(SOCKET server_fd) {
+    sockaddr_in client_addr = {};
+    int client_len = sizeof(client_addr);
+    SOCKET client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
+
+    if (client_fd == INVALID_SOCKET) {
+        if (WSAGetLastError() != WSAEWOULDBLOCK) {
+            std::cerr << "accept() failed with error: " << WSAGetLastError() << std::endl;
+        }
+        return;
+    }
+
+    if (clients.size() >= MAX_CLIENTS) {
+        std::cerr << "Max clients limit reached. Rejecting new connection." << std::endl;
+        closesocket(client_fd);
+        return;
+    }
+
+    clients.push_back({ client_fd, client_addr, client_len });
+    FD_SET(client_fd, &allfds);
+    if (client_fd > max_sd) max_sd = client_fd;
+
+
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+    std::cout << "New TCP connection from " << client_ip << ":"
+        << ntohs(client_addr.sin_port) << std::endl;
+
+    total_connections++;
+}
+
 // Парсер команд и формирование ответа
 string processCommand(const string& cmd) {
     if (cmd == "/time") {
@@ -85,7 +163,6 @@ string processCommand(const string& cmd) {
     }
 }
 
-#include <set>
 void handleUdpClient(SOCKET udp_fd) {
     char buffer[BUFFER_SIZE];
     sockaddr_in client_addr{};
@@ -150,19 +227,26 @@ int main() {
         return 1;
     }
     SOCKET udp_server = createUdpServer(50000);
+    SOCKET tcp_server = createTcpServer(8080);
 
     FD_ZERO(&allfds);
+    FD_SET(tcp_server, &allfds);
     FD_SET(udp_server, &allfds);
-    max_sd = udp_server;
+    max_sd = max(tcp_server, udp_server);
 
-    cout << "UDP Server running. Waiting for connections...\n";
+    cout << "UDP/TCP Server running. Waiting for connections...\n";
 
     while (true) {
         readfds = allfds;
 
-        if (select(0, &readfds, nullptr, nullptr, nullptr) == SOCKET_ERROR) {
-            cerr << "select() failed with error: " << WSAGetLastError() << endl;
+        if (select((int)max_sd + 1, &readfds, nullptr, nullptr, nullptr) == SOCKET_ERROR) {
+            std::cerr << "select() failed with error: " << WSAGetLastError() << std::endl;
             break;
+        }
+
+        // Обработка TCP-сервера (новые подключения)
+        if (FD_ISSET(tcp_server, &readfds)) {
+            acceptTcpConnection(tcp_server);
         }
 
         //  Обработка UDP-сервера
@@ -173,6 +257,7 @@ int main() {
 
     //  Закрытие сокетов
     closesocket(udp_server);
+    closesocket(tcp_server);
     WSACleanup();
     return 0;
 }
